49dbad31f61fbe5a0b8385bec5594a762f7dd524
steve <stevefrazee123@gmail.com>
2021-12-09 15:29:18 -0500

    day 5

---
+++ day_5/hydrothermal.py
@@ -0,0 +1,85 @@
+def IsHorizontal(point_1, point_2):
+    return point_1[1] == point_2[1]
+
+def IsVertical(point_1, point_2):
+    return point_1[0] == point_2[0]
+
+with open("hydrothermal_input.txt") as f:
+    line = f.readline()
+
+    # dictionary where
+    # key = covered point
+    # val = num times it is covered
+    two_or_more = 0
+    covered_points = {}
+    while line:
+        # convert each point to a tuple of x,y ints
+        points = line.split("->")
+        point_1 = points[0].split(',')
+        point_2 = points[1].split(',')
+        point_1 = tuple([int(p.rstrip()) for p in point_1])
+        point_2 = tuple([int(p.rstrip()) for p in point_2])
+
+        if IsHorizontal(point_1, point_2):
+            # walk through x vals and add to covered points
+            x_start = 0
+            x_end   = 0
+            if point_1[0] < point_2[0]:
+                x_start = point_1[0]
+                x_end   = point_2[0]
+            else:
+                x_start = point_2[0]
+                x_end   = point_1[0]
+            y = point_1[1]
+            while x_start <= x_end:
+                t = tuple([x_start,y])
+                if t in covered_points:
+                    covered_points[t] += 1
+                else:
+                    covered_points[t] = 1
+                x_start += 1
+        elif IsVertical(point_1, point_2):
+            # walk through y vals and add to covered points
+            y_start = 0
+            y_end   = 0
+            if point_1[1] < point_2[1]:
+                y_start = point_1[1]
+                y_end   = point_2[1]
+            else:
+                y_start = point_2[1]
+                y_end   = point_1[1]
+            x = point_1[0]
+            while y_start <= y_end:
+                #print(y_start)
+                #print(y_end)
+                t = tuple([x,y_start])
+                if t in covered_points:
+                    covered_points[t] += 1
+                else:
+                    covered_points[t] = 1
+                y_start += 1
+        else:
+            dy = point_2[1] - point_1[1]
+            dx = point_2[0] - point_1[0]
+            dy = 1 if dy > 0 else -1
+            dx = 1 if dx > 0 else -1
+            x_start = point_1[0]
+            y_start = point_1[1]
+            x_end = point_2[0]
+            y_end = point_2[1]
+            for i in range(abs(point_2[0] - point_1[0]) + 1):
+                t = tuple([x_start,y_start])
+                if t in covered_points:
+                    covered_points[t] += 1
+                else:
+                    covered_points[t] = 1
+                x_start += dx
+                y_start += dy
+        line = f.readline()
+
+    # check dictionary for all points with coverage >= 2
+    count = 0
+    for k,v in covered_points.items():
+        if v >= 2:
+            count += 1
+    print(count)
